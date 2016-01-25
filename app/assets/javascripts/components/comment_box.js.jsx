e0380e9ef7a2a2205d36e6d013f194a515e293ed
Emily Scanlon <emily@Emilys-MacBook-Pro.local>
2016-01-25 09:56:17 -0800

    adds more react

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -1,11 +1,31 @@
 // commentTutorial.js
 
 var CommentBox = React.createClass({
+	loadCommentsFromServer: function () {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+    	});
+	},
+	getInitialState = function () {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
 	render: function () {
 		return (
 			<div className="commentBox">
 			<h2>Comments</h2>
-			<commentList />
+			<commentList data={this.state.data} />
 			<commentForm />
 			</div>
 			);
@@ -14,33 +34,55 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
 	render: function () {
+		var commentNodes = this.props.data.map(function(comment) {
+			return (
+				<Comment author={comment.author} key={comment.id}>
+					{comment.text}
+				</Comment>
+			);
+		});
 		return (
 			<div className="commentList">
-				<Comment author="Author1">Comment1</Comment>
-				<Comment author="Author2">Comment2</Comment>
+				{commentNodes}
 			</div>
 			);
 	}
 });
 
 var CommentForm = React.createClass({
+	getInitialState: function () {
+		return {author: '', text: ''};
+	},
+	handleAuthorState: function () {
+		this.setState({author: e.target.value});
+	},
+	handleTextChange: function () {
+		this.setState({text: e.target.value});	
+	},
 	render: function () {
 		return (
-			<div className="commentForm">
-			Hello World, I am a comment form.
-			</div>
+			<form className="commentForm">
+				<input type="text" placeholder="Your name" />
+				<input type="text" placeholder="Content" />
+				<input type="submit" value="Post" />
+			</form>
 			);
 	}
 });
 
-var Comment =React.createClass({
+var Comment = React.createClass({
+	rawMarkup: function () {
+		var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+		return { __html: rawMarkup };
+	},
+
 	render: function () {
 		return (
 			<div className="comment">
 				<h2 className="commentAuthor">
 					{this.props.author}
 				</h2>
-				{marked(this.props.children.toString())}
+				<span dangerouslySetInnerHTML={this.rawMarkup()} />
 			</div>
 			);
 	}
